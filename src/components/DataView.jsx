9fb5c1f1172086d9adbbf84713fae1edb5d8d0cd
Kazuto0p <122356200+Kazuto0p@users.noreply.github.com>
2024-04-28 19:41:23 +0530

    Add files via upload

---
+++ src/components/DataView.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap'; // Import Button from react-bootstrap
+
+const DataView = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the server endpoint
+    axios.get("http://localhost:8080/view")
+      .then(response => {
+        setData(response.data); // Set fetched data to state
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="center" gap={2}>
+      {data.map((val,i)=> (
+        <Card   key={val._id} sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={val.imageUrl}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {val.fname}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Price: {val.fprice}
+              </Typography>
+              <Link to="/Payment" style={{ textDecoration: 'none' }}>
+                <Button variant="contained" color="primary">
+                  Buy Now
+                </Button>
+              </Link>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
+  );
+};
+
+export default DataView;
